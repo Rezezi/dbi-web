95f65fc121795045f6f62a831031ffde31cbb8ce
Aziz Khasyi <azizkhasyi11@gmail.com>
2024-12-05 12:04:07 +0700

    feat: Add Tailwind CSS and React Router setup with Home page component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export const Home = () => {
+  return (
+    <div className='bg-gray-100 h-screen flex justify-center items-center'>
+      <h1 className='text-4xl font-bold text-gray-800'>Hello World</h1>
+    </div>
+  )
+}
